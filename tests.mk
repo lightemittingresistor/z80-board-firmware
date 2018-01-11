d768c658b9f9d4cc342caea057cb140ce8ab7193
Michael Cullen <michael@cullen-online.com>
2018-01-11 15:19:24 +0000

    remove serial test - it was tricky to maintain and not current anyway

--- tests.mk
+++ tests.mk
@@ -1,10 +1,5 @@
 # Makfile fragment for PC-based tests
 
-SERIAL_TEST_SOURCES = \
-	tests/serial_tests.c \
-	src/serial.c \
-	$(CURDIR)/tests/mock_include/mock_include.c
-
 INTERNAL_TEST_SOURCES = \
 	tests/internal_tests.c \
 	$(CURDIR)/tests/mock_include/mock_include.c
@@ -13,15 +8,11 @@ TESTS_CFLAGS = -g -DF_CPU=${FREQUENCY}UL -DPC_TEST -I$(CURDIR)/include -I$(CURDI
 
 TEST_CC=gcc
 
-OBJECTS_SERIAL_TEST = $(SERIAL_TEST_SOURCES:%.c=build-tests/%.o)
 OBJECTS_INTERNAL_TEST = $(INTERNAL_TEST_SOURCES:%.c=build-tests/%.o)
 
 build-tests/%.o : %.c
 	mkdir -p `dirname $@`
 	${TEST_CC} -c ${TESTS_CFLAGS} $< -o $@
-
-build-tests/serial_tests : ${OBJECTS_SERIAL_TEST}
-	${TEST_CC}  ${TESTS_CFLAGS} -o $@ $^
 
 build-tests/internal_tests : ${OBJECTS_INTERNAL_TEST}
 	${TEST_CC}  ${TESTS_CFLAGS} -o $@ $^
@@ -29,7 +20,4 @@ build-tests/internal_tests : ${OBJECTS_INTERNAL_TEST}
 internal_tests: build-tests/internal_tests
 	build-tests/internal_tests
 
-serial_tests: build-tests/serial_tests
-	build-tests/serial_tests
-
-check : internal_tests serial_tests
+check : internal_tests
