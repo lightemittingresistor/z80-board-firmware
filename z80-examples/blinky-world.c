3a1f7300ab6440fe0e994a17aa534daaac04b050
Michael Cullen <michael@cullen-online.com>
2018-12-17 18:45:59 +0000

    Begin updaing for V2 board

--- z80-examples/blinky-world.c
+++ z80-examples/blinky-world.c
@@ -7,9 +7,6 @@
 */
 
 #define pio_a_d 0x0
-#define pio_b_d 0x01
-#define pio_a_c 0x02
-#define pio_b_c 0x03
 
 void main()
 {
@@ -17,27 +14,7 @@ void main()
 	{	
 		#asm
 
-		ld a, 0xcf
-    out (pio_a_c), a ; send 0xcf to A (control mode)
-    ld a, 0x00
-    out (pio_a_c), a ; set A to output (0)
-    ld a, 0xcf
-    out (pio_b_c), a ; send 0xcf to B (control mode)
-    ld a, 0xff
-    out (pio_b_c), a ; set B to input (1)
-
-    ; disable interupts
-    ld a, 0x07
-    out (pio_a_c), a
-    out (pio_b_c), a 
-
-    ; turn LEDs on
-    ld a, 0xff
-    out (pio_a_d), a ; set outputs to on
-
-
-		ld a, 0xff
-		out (0x08), a
+		
 		ld a, 0x0f
 		out (pio_a_d), a
 		#endasm
